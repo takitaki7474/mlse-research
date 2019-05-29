1778c3fcde7157fd6b1059ab88484bc726a2f91e
takitaki7474 <shark747474@gmail.com>
2019-05-29 17:39:44 +0900

    add search img func

--- processing_table.py
+++ processing_table.py
@@ -20,8 +20,19 @@ class processing_feature_table():
             feature_sum_values.append(sum(i))
         self.feature_table["feature_sum"] = feature_sum_values
 
+    def add_feature_highorder_sum(self, highorder):
+        column_name = "highorder_sum_" + str(highorder)
+        highorder_sum = []
+        for i in self.feature["feature"]:
+            sorted_feature = sorted(i, reverse=True)
+            highorder_sum.append(sum(sorted_feature[:highorder]))
+        self.feature[column_name] = highorder_sum
+
     def feature_table_sort(self,sort_column):
         self.feature = self.feature.sort_values(by=[sort_column], ascending=True)
         self.feature = self.feature.reset_index()
 
-    
+    def search_img(self,img_num):
+        images = list(self.feature["img"][:img_num])
+
+        return images
